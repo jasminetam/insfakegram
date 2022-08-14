b508c9c3f724fe12bacadc46435f259d14c715aa
jasminetam <jasminetam94@gmail.com>
2022-08-14 15:34:50 +0100

    circleci and jest setup

---
+++ __test__/Header.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import  Header  from '../components/Header';
+
+
+describe("Header test", () => {
+  it("expect Header component is rendered without crashing", () => {
+    const wrapper = shallow(<Header />);
+  });
+  it("expect Header component is rendered", () => {
+    const wrapper = shallow(<Header />);
+  });
+
+})
